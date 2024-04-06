74c611fcb7110c6542026dad370e3fd6c07e561b
Fikayo <demixwillex@gmail.com>
2024-04-06 08:08:32 +0100

    Modal Window

--- starter/script.js
+++ starter/script.js
@@ -5,17 +5,20 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btn = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(
-    btnsOpenModal[i].addEventListener('click', function () {
-      console.log('button clicked');
-      modal.classList.remove('hidden');
-      overlay.classList.remove('hidden');
-    })
-  );
-btnCloseModal.addEventListener('click', function () {
+const openModal = function () {
+  console.log('button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
